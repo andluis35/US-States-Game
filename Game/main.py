d0a6b57aaf24163d09f770f0a6860a550e85e24d
Anderson Luis <and.luis35@gmail.com>
2025-01-21 07:15:02 -0300

    Adicionada funcionalidade de escrever um arquivo csv com os Estados que faltaram

--- Game/main.py
+++ Game/main.py
@@ -12,6 +12,28 @@ def write_state(land, data):
     state_name.goto(x=state_data.x.item(), y=state_data.y.item())
     state_name.write(land)
 
+def set_player_guess(game_screen, score):
+    if score == 0:
+        return game_screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+    else:
+        updated_score = f"{right_guesses}/{NUMBER_OF_STATES} States Correct"
+        return game_screen.textinput(title=updated_score, prompt="What's another state's name?").title()
+
+def check_for_missing_states(states, written_states):
+    missing_states_dictionary = {
+        "state": []
+    }
+
+    for land in states["state"]:
+        if land not in written_states:
+            missing_states_dictionary["state"].append(land)
+
+    return missing_states_dictionary
+
+def create_csv_file_with_missing_states(missing_states_dictionary):
+    missing_states_data = pandas.DataFrame(missing_states_dictionary)
+    missing_states_data.to_csv("./missing_states.csv")
+
 screen = turtle.Screen()
 screen.setup(width=725, height=491)
 screen.title("U.S. States Game")
@@ -22,13 +44,13 @@ turtle.shape(image)
 states_file = pandas.read_csv("./50_states.csv")
 right_guesses = 0
 guessed_states = []
+missing_states = {}
 
 while right_guesses < NUMBER_OF_STATES:
-    if right_guesses == 0:
-        answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
-    else:
-        updated_score = f"{right_guesses}/{NUMBER_OF_STATES} States Correct"
-        answer_state = screen.textinput(title=updated_score, prompt="What's another state's name?").title()
+    answer_state = set_player_guess(screen, right_guesses)
+
+    if answer_state == "Exit":
+        break
 
     for state in states_file["state"]:
         if (answer_state == state) and (answer_state not in guessed_states):
@@ -36,4 +58,5 @@ while right_guesses < NUMBER_OF_STATES:
             write_state(state, states_file)
             guessed_states.append(state)
 
-screen.exitonclick()
+missing_states = check_for_missing_states(states_file, guessed_states)
+create_csv_file_with_missing_states(missing_states)
